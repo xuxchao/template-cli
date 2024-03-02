1708d265145320a4b95aac3bd89d23e600a1da85
xuxchao <645352780@qq.com>
2024-03-02 10:56:11 +0800

    feat: 添加文件路径信息

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::io::Write;
 use std::path::PathBuf;
 use zip::read::ZipArchive;
 
+/// 弹出一层路径
+/// 原路径:　/a/b/c
+/// 处理后: /b/c
 fn unshift_path(path: &PathBuf) -> PathBuf {
     let mut components = path.components();
 
@@ -16,12 +19,33 @@ fn unshift_path(path: &PathBuf) -> PathBuf {
     components.as_path().to_path_buf()
 }
 
+/// 在最前面插入一层路径一层路径
+///
+/// # example
+/// ```
+/// shift_path("/c", "/a/b") // /c/a/b
+/// ```
 fn shift_path(path: &str, path_but: &PathBuf) -> PathBuf {
     let mut n = PathBuf::new();
     n = n.join(path);
     n.join(path_but)
 }
 
+/// 解压文件夹到目标文件夹
+///
+/// path 路径, 从这个路径读取压缩文件
+/// to_path 目标路径，将文件解压的路径
+/// unshift 取消一级路径
+///
+/// # example
+/// test.zip => test/xxx/实际内容
+/// ```
+/// unzip("test.zip", "toTest", true)
+/// test.zip => test/实际内容
+///
+/// unzip("test.zip", "toTest", false)
+/// test.zip => test/xxx/实际内容
+/// ```
 fn unzip(path: &str, to_path: &str, unshift: bool) -> zip::result::ZipResult<()> {
     let zip_file = File::open(path)?;
     let mut zip = ZipArchive::new(zip_file)?;
@@ -49,6 +73,29 @@ fn unzip(path: &str, to_path: &str, unshift: bool) -> zip::result::ZipResult<()>
     Ok(())
 }
 
+/// 下载 github 仓库的 zip 包
+///
+/// archive_url github 仓库直接下载 main.zip 包的地址
+/// name 下载的压缩包的名字
+async fn download_github_zip(
+    archive_url: &str,
+    name: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    // Create a reqwest client
+    // Send a GET request to download the archive
+    let response = reqwest::get(archive_url).await?.error_for_status()?;
+
+    // 获取响应体的字节
+    let bytes = response.bytes().await?;
+
+    // 创建一个用于保存 ZIP 文件的 File 实例
+    let mut file = File::create(name)?;
+    // 将字节写入文件
+    file.write_all(&bytes)?;
+
+    Ok(())
+}
+
 /// 对指定目录生成模板文件
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -57,7 +104,6 @@ struct Args {
     #[arg(short, long)]
     path: String,
 }
-// async fn start() -> Result<(), Box<dyn std::error::Error>> {}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -74,26 +120,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     download_github_zip(archive_url, zip_path).await?;
 
     unzip(&zip_path, &path, true)?;
-
-    Ok(())
-}
-
-// 下载 github 仓库的 zip 包
-async fn download_github_zip(
-    archive_url: &str,
-    name: &str,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // Create a reqwest client
-    // Send a GET request to download the archive
-    let response = reqwest::get(archive_url).await?.error_for_status()?;
-
-    // 获取响应体的字节
-    let bytes = response.bytes().await?;
-
-    // 创建一个用于保存 ZIP 文件的 File 实例
-    let mut file = File::create(name)?;
-    // 将字节写入文件
-    file.write_all(&bytes)?;
 
     Ok(())
 }
